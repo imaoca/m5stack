4a05b411aa14cc5b7b0874d6703f349ca45d828c
michihiro imaoka <imaoca@gmail.com>
2019-03-29 21:58:48 +0900

    Add files via upload

---
+++ m5LedSub.ino
@@ -0,0 +1,101 @@
+/*
+reference:
+http://shuzo-kino.hateblo.jp/entry/2017/10/11/235624
+*/
+#include <WiFi.h>
+#include <PubSubClient.h>
+const char* ssid = "**********";
+const char* password = "**********";
+const char* mqtt_server = "**********";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(5, LOW); 
+    Serial.println("coming here");
+  } else {
+    digitalWrite(5, HIGH); 
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      digitalWrite(5,HIGH);
+      client.subscribe("msg/led");
+      delay(1000);
+      digitalWrite(5,LOW);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(5, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(2, OUTPUT);
+  digitalWrite(2,LOW);
+  digitalWrite(5,HIGH);
+  Serial.begin(115200);
+  setup_wifi();
+  digitalWrite(5,LOW);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
